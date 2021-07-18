3dac72587de94d415de3bcae10ccc4854021328e
Aditya <aditya26sg@gmail.com>
2021-07-19 01:18:07 +0530

    comments update in plots_utility

--- home/plots_utility.py
+++ home/plots_utility.py
@@ -5,11 +5,14 @@ from plotly.graph_objs import Candlestick
 
 
 def plot_candlestick(data, ticker):
-    layout = dict(
-            title = ticker,
-            xaxis = go.layout.XAxis(title = go.layout.xaxis.Title(text = "Time")),
-            yaxis = go.layout.YAxis(title = go.layout.yaxis.Title(text = "Price US $"))
-        )
+
+    ########## This is used to get the plots on a new webpage not on the same html page.
+
+    # layout = dict(
+    #         title = ticker,
+    #         xaxis = go.layout.XAxis(title = go.layout.xaxis.Title(text = "Time")),
+    #         yaxis = go.layout.YAxis(title = go.layout.yaxis.Title(text = "Price US $"))
+    #     )
 
     # plot_d = [go.Candlestick(x = data.index,
     #             open = data['Open'],
@@ -18,6 +21,8 @@ def plot_candlestick(data, ticker):
     #             close = data['Close'])]
     # plot_div = go.Figure(data = plot_d, layout = layout)
 
+
+    ########## This is used to get the plots on the same webpage.
     plot_div = plot([Candlestick(x = data.index, 
                     open=data['Open'], 
                     high = data['High'],
