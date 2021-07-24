a6dc5e4610dc93fb027de4a68eab7da6734902fe
Aditya <aditya26sg@gmail.com>
2021-07-24 14:34:18 +0530

    Multiplots

--- home/plots_utility.py
+++ home/plots_utility.py
@@ -1,40 +1,85 @@
 import plotly.graph_objs as go
 from plotly.offline import plot
 from plotly.graph_objs import Candlestick
+import plotly.express as px
 
 
+class Plotting_graphs:
+    def __init__(self, data, ticker):
+        self.data = data
+        self.ticker = ticker
+        self.layout = dict(
+                title = ticker,
+                xaxis = dict(
+                    rangeselector=dict(
+                        buttons = list([
+                            dict(count=1,
+                                label = "1m",
+                                step = "month",
+                                stepmode = "backward"),
+                            dict(count=6,
+                                label = "6m",
+                                step = "month",
+                                stepmode = "backward"),
+                            dict(count=1,
+                                label = "YTD",
+                                step = "year",
+                                stepmode = "todate"),
+                            dict(count=1,
+                                label = "1y",
+                                step = "year",
+                                stepmode = "backward"),
+                            dict(step="all")
+                        ])
+                    ),
+                    rangeslider=dict(visible=True),
+                    type = "date"
+                ),
+                yaxis = go.layout.YAxis(title = go.layout.yaxis.Title(text = "Price US $")),
+                height=700,
+            )
 
-def plot_candlestick(data, ticker):
 
-    ########## This is used to get the plots on a new webpage not on the same html page.
+    def plot_candlestick(self):
 
-    layout = dict(
-            title = ticker,
-            xaxis = go.layout.XAxis(title = go.layout.xaxis.Title(text = "Time")),
-            yaxis = go.layout.YAxis(title = go.layout.yaxis.Title(text = "Price US $")),
-            height=700,
-        )
+        ########## This is used to get the plots on a new webpage not on the same html page.
 
-    plot_d = [go.Candlestick(x = data.index,
-                open = data['Open'],
-                high = data['High'],
-                low = data['Low'],
-                close = data['Close'])]
-    plot_div = go.Figure(data = plot_d, layout = layout)
-    
-    
+        plot_d = [go.Candlestick(x = self.data.index,
+                    open = self.data['Open'],
+                    high = self.data['High'],
+                    low = self.data['Low'],
+                    close = self.data['Close'])]
+        plot_div = go.Figure(data = plot_d, layout = self.layout)
+        
+        
 
-    # This line of code can take the plot_d and the layout together and plot the graph offline on the same page
-    plot_html = plot(plot_div, output_type='div', include_plotlyjs=False, show_link=False)
+        # This line of code can take the plot_d and the layout together and plot the graph offline on the same page
+        plot_html = plot(plot_div, output_type='div', include_plotlyjs=False, show_link=False)
 
-    ########## This is used to get the plots on the same webpage.
-    # plot_div = plot([Candlestick(x = data.index, 
-    #                 open=data['Open'], 
-    #                 high = data['High'],
-    #                 low = data['Low'],
-    #                 close = data['Close'],
-    #                 name='Candlestick Plot', opacity=0.8,
-    #             )], output_type='div', include_plotlyjs=False, show_link=False)
-    # print('PLot done')
+        ########## This is used to get the plots on the same webpage.
+        # plot_div = plot([Candlestick(x = data.index, 
+        #                 open=data['Open'], 
+        #                 high = data['High'],
+        #                 low = data['Low'],
+        #                 close = data['Close'],
+        #                 name='Candlestick Plot', opacity=0.8,
+        #             )], output_type='div', include_plotlyjs=False, show_link=False)
+        # print('PLot done')
 
-    return plot_html
+        return plot_html
+
+    def plot_scatter(self):
+        fig = go.Figure([go.Scatter(x = self.data.index, y=self.data['Close'])], layout = self.layout)
+        plot_html = plot(fig, output_type='div', include_plotlyjs=False, show_link=False)
+        return plot_html
+
+    def plot_bar(self):
+        fig = go.Figure([go.Bar(x = self.data.index, y=self.data['Close'])], layout = self.layout)
+        # fig = px.bar(self.data, x = self.data.index, y=self.data['Close'], layout=self.layout)
+        plot_html = plot(fig, output_type='div', include_plotlyjs=False, show_link=False)
+        return plot_html
+
+    def area_plot(self):
+        fig = px.area(self.data, x=self.data.index, y=self.data['Close'])
+        plot_html = plot(fig, output_type='div', include_plotlyjs=False, show_link=False)
+        return plot_html
--- home/views.py
+++ home/views.py
@@ -4,7 +4,7 @@ from plotly.offline import plot
 from plotly.graph_objs import Scatter
 from .forms import StockQueryForm
 from .stock_utility import get_stock_data
-from .plots_utility import plot_candlestick
+from .plots_utility import Plotting_graphs
 
 def indexplot(request):
     xdata = [0,1,2,3,4,5]
@@ -27,7 +27,18 @@ class GetStockData(TemplateView):
     def post(self, request):
         form = StockQueryForm(request.POST)
         if form.is_valid():
-            data = get_stock_data(request.POST['start'], request.POST['end'], request.POST['ticker'])
-            plot_div = plot_candlestick(data, request.POST['ticker'])
-        args = {'form': form, 'plot_div': plot_div}
+            ticker = request.POST['ticker']
+            data = get_stock_data(request.POST['start'], request.POST['end'], ticker)
+            pg = Plotting_graphs(data, ticker)
+            bar_plot_div = pg.plot_bar()
+            candlestick_plot_div = pg.plot_candlestick()
+            scatter_plot_div = pg.plot_scatter()
+            area_plot_div = pg.area_plot()
+        args = {
+                'form': form, 
+                'bar_plot_div': bar_plot_div,
+                'candlestick_plot_div': candlestick_plot_div,
+                'scatter_plot_div': scatter_plot_div,
+                'area_plot_div': area_plot_div
+            }
         return render(request, self.template_name, args)
